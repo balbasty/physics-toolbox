fbb900778f7ffd8e5d965555b4ae9dea2dcda59b
Yaël Balbastre <yael.balbastre@gmail.com>
2018-11-08 14:23:35 +0000

    Enhancement: convert xml to structure + use it to make read cleaner
    + default modes to extract specific subparts (autocalibration/cartesian sampling)

--- ismrmrd_read.m
+++ ismrmrd_read.m
@@ -16,6 +16,7 @@ function dat = ismrmrd_read(fname, varargin)
 % repetition           - Indices of repetitions to read
 % set                  - Indices of sets to read
 % segment              - Indices of segments to read
+% subpart              - Specific cases ('autocalib'/'cartesian'/'caipi')
 %
 % OPTIONS (KEYWORDS)
 % ------------------
@@ -36,13 +37,16 @@ function dat = ismrmrd_read(fname, varargin)
 %           9: sets
 %          10: segments
 
+% -------------------------------------------------------------------------
+% Prepare header information
+head = struct;
 
 % -------------------------------------------------------------------------
 % Parse input arguments
 p = inputParser;
 p.FunctionName = 'ismrmrd_read';
 p.addParameter('channel',               Inf, @isnumeric);
-p.addParameter('readout',               Inf, @isnumeric);
+p.addParameter('readout',               Inf, @(X) isnumeric(X) || ischar(X));
 p.addParameter('kspace_encode_step_1',  Inf, @isnumeric);
 p.addParameter('kspace_encode_step_2',  Inf, @isnumeric);
 p.addParameter('average',               Inf, @isnumeric);
@@ -53,28 +57,60 @@ p.addParameter('repetition',            Inf, @isnumeric);
 p.addParameter('set',                   Inf, @isnumeric);
 p.addParameter('segment',               Inf, @isnumeric);
 p.addParameter('compact',             false, @islogical);
-p.addParameter('maxmem',               5E8, @islogical);
+p.addParameter('maxmem',                5E8, @islogical);
+p.addParameter('subpart',                '', @ischar);
 p.parse(varargin{:});
-k1 = p.Results.kspace_encode_step_1;
-k2 = p.Results.kspace_encode_step_2;
-av = p.Results.average;
-sl = p.Results.slice;
-ct = p.Results.contrast;
-ph = p.Results.phase;
-rp = p.Results.repetition;
-st = p.Results.set;
-sg = p.Results.segment;
-rd = p.Results.readout;
-ch = p.Results.channel;
-compact = p.Results.compact;
-maxmem  = p.Results.maxmem;
+head.k1.out = p.Results.kspace_encode_step_1;
+head.k2.out = p.Results.kspace_encode_step_2;
+head.av.out = p.Results.average;
+head.sl.out = p.Results.slice;
+head.ct.out = p.Results.contrast;
+head.ph.out = p.Results.phase;
+head.rp.out = p.Results.repetition;
+head.st.out = p.Results.set;
+head.sg.out = p.Results.segment;
+head.rd.out = p.Results.readout;
+head.ch.out = p.Results.channel;
+compact     = p.Results.compact;
+maxmem      = p.Results.maxmem;
+subpart     = p.Results.subpart;
 
 % -------------------------------------------------------------------------
 % Read soft header
-% fprintf('Read soft header\n');
-% softhead = ismrmrd_xml(fname);
-info     = h5info(fname, '/dataset/data');
-nlines   = info.Dataspace.Size;
+infoh5   = h5info(fname, '/dataset/data');
+nlines   = infoh5.Dataspace.Size;
+fprintf('Read soft header\n');
+softhead = ismrmrd_xml(fname);
+
+head_limits = softhead.ismrmrdHeader.encoding.encodingLimits;
+
+head.k1.lim.min    = 1 + str2double(head_limits.kspace_encoding_step_1.minimum.get_data);
+head.k1.lim.max    = 1 + str2double(head_limits.kspace_encoding_step_1.maximum.get_data);
+head.k1.lim.center = 1 + str2double(head_limits.kspace_encoding_step_1.center.get_data);
+head.k2.lim.min    = 1 + str2double(head_limits.kspace_encoding_step_2.minimum.get_data);
+head.k2.lim.max    = 1 + str2double(head_limits.kspace_encoding_step_2.maximum.get_data);
+head.k2.lim.center = 1 + str2double(head_limits.kspace_encoding_step_2.center.get_data);
+head.av.lim.min    = 1 + str2double(head_limits.average.minimum.get_data);
+head.av.lim.max    = 1 + str2double(head_limits.average.maximum.get_data);
+head.av.lim.center = 1 + str2double(head_limits.average.center.get_data);
+head.sl.lim.min    = 1 + str2double(head_limits.slice.minimum.get_data);
+head.sl.lim.max    = 1 + str2double(head_limits.slice.maximum.get_data);
+head.sl.lim.center = 1 + str2double(head_limits.slice.center.get_data);
+head.ct.lim.min    = 1 + str2double(head_limits.contrast.minimum.get_data);
+head.ct.lim.max    = 1 + str2double(head_limits.contrast.maximum.get_data);
+head.ct.lim.center = 1 + str2double(head_limits.contrast.center.get_data);
+head.ph.lim.min    = 1 + str2double(head_limits.phase.minimum.get_data);
+head.ph.lim.max    = 1 + str2double(head_limits.phase.maximum.get_data);
+head.ph.lim.center = 1 + str2double(head_limits.phase.center.get_data);
+head.rp.lim.min    = 1 + str2double(head_limits.repetition.minimum.get_data);
+head.rp.lim.max    = 1 + str2double(head_limits.repetition.maximum.get_data);
+head.rp.lim.center = 1 + str2double(head_limits.repetition.center.get_data);
+head.st.lim.min    = 1 + str2double(head_limits.set.minimum.get_data);
+head.st.lim.max    = 1 + str2double(head_limits.set.maximum.get_data);
+head.st.lim.center = 1 + str2double(head_limits.set.center.get_data);
+head.sg.lim.min    = 1 + str2double(head_limits.segment.minimum.get_data);
+head.sg.lim.max    = 1 + str2double(head_limits.segment.maximum.get_data);
+head.sg.lim.center = 1 + str2double(head_limits.segment.center.get_data);
 
 % -------------------------------------------------------------------------
 % Reading strategy
@@ -86,15 +122,15 @@ linestep = floor(maxmem/(linelength*4));
 % -------------------------------------------------------------------------
 % Read available indices
 fprintf('Read hard header:   0%%');
-k1steps     = [];
-k2steps     = [];
-averages    = [];
-slices      = [];
-contrasts   = [];
-phases      = [];
-repetitions = [];
-sets        = [];
-segments    = [];
+head.k1.smp = [];
+head.k2.smp = [];
+head.av.smp = [];
+head.sl.smp = [];
+head.ct.smp = [];
+head.ph.smp = [];
+head.rp.smp = [];
+head.st.smp = [];
+head.sg.smp = [];
 pct_prev    = 0;
 for i=1:linestep:nlines
     pct = floor(100*i/nlines);
@@ -104,49 +140,94 @@ for i=1:linestep:nlines
     end
     nread       = min(linestep, nlines-i);
     dat1        = h5read(fname, '/dataset/data', i, nread);
-    k1steps     = unique([k1steps       dat1.head.idx.kspace_encode_step_1(:)']);
-    k2steps     = unique([k2steps       dat1.head.idx.kspace_encode_step_2(:)']);
-    averages    = unique([averages      dat1.head.idx.average(:)']);
-    slices      = unique([slices        dat1.head.idx.slice(:)']);
-    contrasts   = unique([contrasts     dat1.head.idx.contrast(:)']);
-    phases      = unique([phases        dat1.head.idx.phase(:)']);
-    repetitions = unique([repetitions   dat1.head.idx.repetition(:)']);
-    sets        = unique([sets          dat1.head.idx.set(:)']);
-    segments    = unique([segments      dat1.head.idx.segment(:)']);
+    head.k1.smp = unique([head.k1.smp dat1.head.idx.kspace_encode_step_1(:)']);
+    head.k2.smp = unique([head.k2.smp dat1.head.idx.kspace_encode_step_2(:)']);
+    head.av.smp = unique([head.av.smp dat1.head.idx.average(:)']);
+    head.sl.smp = unique([head.sl.smp dat1.head.idx.slice(:)']);
+    head.ct.smp = unique([head.ct.smp dat1.head.idx.contrast(:)']);
+    head.ph.smp = unique([head.ph.smp dat1.head.idx.phase(:)']);
+    head.rp.smp = unique([head.rp.smp dat1.head.idx.repetition(:)']);
+    head.st.smp = unique([head.st.smp dat1.head.idx.set(:)']);
+    head.sg.smp = unique([head.sg.smp dat1.head.idx.segment(:)']);
 end
 fprintf('\n');
 % Convert to matlab 1-indices
-k1steps     = k1steps     + 1;
-k2steps     = k2steps     + 1;
-averages    = averages    + 1;
-slices      = slices      + 1;
-contrasts   = contrasts   + 1;
-phases      = phases      + 1;
-repetitions = repetitions + 1;
-sets        = sets        + 1;
-segments    = segments    + 1;
+head.k1.smp = head.k1.smp + 1;
+head.k2.smp = head.k2.smp + 1;
+head.av.smp = head.av.smp + 1;
+head.sl.smp = head.sl.smp + 1;
+head.ct.smp = head.ct.smp + 1;
+head.ph.smp = head.ph.smp + 1;
+head.rp.smp = head.rp.smp + 1;
+head.st.smp = head.st.smp + 1;
+head.sg.smp = head.sg.smp + 1;
 
 % -------------------------------------------------------------------------
 % Read index range inside each line
-dat1      = h5read(fname, '/dataset/data', 1, 1);
-nreadouts = dat1.head.number_of_samples;
-nchannels = dat1.head.available_channels;
-channels  = 1:nchannels;
-readouts  = 1:nreadouts;
+dat1            = h5read(fname, '/dataset/data', 1, 1);
+head.rd.nb      = dat1.head.number_of_samples;
+head.rd.lim.min = 1;
+head.rd.lim.max = head.rd.nb;
+head.rd.smp     = head.rd.lim.min:head.rd.lim.max;
+head.ch.nb      = dat1.head.available_channels;
+head.ch.lim.min = 1;
+head.ch.lim.max = head.ch.nb;
+head.ch.smp     = head.ch.lim.min:head.ch.lim.max;
+
+% -------------------------------------------------------------------------
+% Specific subparts
+switch subpart
+    case 'autocalib'
+        try
+            ack1name       = 'EmbeddedRefLinesE1';
+            ack2name       = 'EmbeddedRefLinesE2';
+            userparams     = {softhead.ismrmrdHeader.userParameters.userParameterLong.name};
+            userparams     = cellfun(@(x) char(x.get_data), userparams, 'UniformOutput', false);
+            uservalues     = {softhead.ismrmrdHeader.userParameters.userParameterLong.value};
+            head.k1.ac.nb  = str2double(uservalues{strcmpi(userparams,ack1name)}.get_data);
+            head.k2.ac.nb  = str2double(uservalues{strcmpi(userparams,ack2name)}.get_data);
+            head.k1.ac.min = head.k1.lim.center - head.k1.ac.nb/2;
+            head.k1.ac.max = head.k1.lim.center + head.k1.ac.nb/2 - 1;
+            head.k2.ac.min = head.k2.lim.center - head.k2.ac.nb/2;
+            head.k2.ac.max = head.k2.lim.center + head.k2.ac.nb/2 - 1;
+            head.k1.out    = head.k1.ac.min:head.k1.ac.max;
+            head.k2.out    = head.k2.ac.min:head.k2.ac.max;
+        catch
+        end
+    case 'cartesian'
+        try
+            head.k1.af  = str2double(softhead.ismrmrdHeader.encoding.parallelImaging.accelerationFactor.kspace_encoding_step_1.get_data);
+            head.k2.af  = str2double(softhead.ismrmrdHeader.encoding.parallelImaging.accelerationFactor.kspace_encoding_step_2.get_data);
+            head.k1.out = head.k1.lim.min:head.k1.af:head.k1.lim.max;
+            head.k2.out = head.k2.lim.min:head.k2.af:head.k2.lim.max;
+        catch
+        end
+end
 
 % -------------------------------------------------------------------------
 % Default indices
-if ch == Inf, ch = channels;    end
-if rd == Inf, rd = readouts;    end
-if k1 == Inf, k1 = k1steps;     end
-if k2 == Inf, k2 = k2steps;     end
-if av == Inf, av = averages;    end
-if sl == Inf, sl = slices;      end
-if ct == Inf, ct = contrasts;   end
-if ph == Inf, ph = phases;      end
-if rp == Inf, rp = repetitions; end
-if st == Inf, st = sets;        end
-if sg == Inf, sg = segments;    end
+if ischar(head.rd.out)
+    if strcmpi(head.rd.out(1), '/')
+        sub         = str2double(head.rd.out(2:end));
+        len         = head.rd.lim.max - head.rd.lim.min + 1;
+        keep        = ceil(len/sub);
+        skip        = floor((len-keep)/2);
+        head.rd.out = (head.rd.lim.min + skip):(head.rd.lim.min + skip + keep - 1);
+    else
+        head.rd.out = Inf;
+    end
+end
+if head.ch.out == Inf, head.ch.out = head.ch.smp; end
+if head.rd.out == Inf, head.rd.out = head.rd.smp; end
+if head.k1.out == Inf, head.k1.out = head.k1.smp; end
+if head.k2.out == Inf, head.k2.out = head.k2.smp; end
+if head.av.out == Inf, head.av.out = head.av.smp; end
+if head.sl.out == Inf, head.sl.out = head.sl.smp; end
+if head.ct.out == Inf, head.ct.out = head.ct.smp; end
+if head.ph.out == Inf, head.ph.out = head.ph.smp; end
+if head.rp.out == Inf, head.rp.out = head.rp.smp; end
+if head.st.out == Inf, head.st.out = head.st.smp; end
+if head.sg.out == Inf, head.sg.out = head.sg.smp; end
 
 % -------------------------------------------------------------------------
 % Allocate output
@@ -160,8 +241,12 @@ compactind = @full2comp;
 expandind  = @(x,y) reshape(x - y(1) + 1, [], 1);
 if compact, getdim = compactdim; getind = compactind;
 else,       getdim = expanddim;  getind = expandind;  end
-dim = [numel(ch) numel(rd) getdim(k1) getdim(k2) numel(av) ...
-       numel(sl) numel(ct) numel(ph)  numel(rp)  numel(st) numel(sg)];
+dim = [numel(head.ch.out)  numel(head.rd.out) ...
+       getdim(head.k1.out) getdim(head.k2.out) ...
+       numel(head.av.out)  numel(head.sl.out) ...
+       numel(head.ct.out)  numel(head.ph.out) ...
+       numel(head.rp.out)  numel(head.st.out) ...
+       numel(head.sg.out)];
 dat = zeros(dim, 'like', single(1i));
 
 % -------------------------------------------------------------------------
@@ -185,7 +270,7 @@ for i=1:linestep:nlines
     st1  = 1 + dat1.head.idx.set(:);
     sg1  = 1 + dat1.head.idx.segment(:);
     
-    includedind = {k1, k2, av, sl, ct, ph, rp, st, sg};
+    includedind = {head.k1.out, head.k2.out, head.av.out, head.sl.out, head.ct.out, head.ph.out, head.rp.out, head.st.out, head.sg.out};
     foundind    = {k11, k21, av1, sl1, ct1, ph1, rp1, st1, sg1};
     isincluded  = @(x,y) any(bsxfun(@eq, reshape(x, [], 1), ...
                                          reshape(y, 1, [])), 2);
@@ -193,14 +278,14 @@ for i=1:linestep:nlines
     msk = all([msk{:}],2);
     
     line = cat(2, dat1.data{msk});
-    line = reshape(line, [2 nreadouts nchannels sum(msk)]);
+    line = reshape(line, [2 head.rd.nb head.ch.nb sum(msk)]);
     line = permute(line(1,:,:,:) + 1i*line(2,:,:,:), [3 2 4 1]);
     
     foundind = [k11, k21, av1, sl1, ct1, ph1, rp1, st1, sg1];
     foundind = foundind(msk,:);
     linind   = sub2ind(dim(3:end), ...
-                       getind(foundind(:,1),includedind{1}), ...
-                       getind(foundind(:,2),includedind{2}), ...
+                           getind(foundind(:,1),includedind{1}), ...
+                           getind(foundind(:,2),includedind{2}), ...
                        compactind(foundind(:,3),includedind{3}), ...
                        compactind(foundind(:,4),includedind{4}), ...
                        compactind(foundind(:,5),includedind{5}), ...
@@ -209,7 +294,7 @@ for i=1:linestep:nlines
                        compactind(foundind(:,8),includedind{8}), ...
                        compactind(foundind(:,9),includedind{9}));
         
-     dat(:,:,linind) = line(ch,rd,:);
+     dat(:,:,linind) = line(head.ch.out,head.rd.out,:);
                    
 end
 fprintf('\n');
--- ismrmrd_xml.m
+++ ismrmrd_xml.m
@@ -1,5 +1,5 @@
-function s = ismrmrd_xml(fname)
-% FORMAT s = ismrmrd_xml(fname)
+function [obj,s] = ismrmrd_xml(fname)
+% FORMAT obj = ismrmrd_xml(fname)
 %
 % Read the flexible header of an ismrmrd file into a structure
 
@@ -8,3 +8,56 @@ xmlstring = xmlstring{1};
 
 s = xmlreadstring(xmlstring);
 
+obj = parseNode(s);
+
+function node = parseNode(theNode)
+% Recurse over node children.
+
+node = struct;
+if theNode.hasChildNodes
+    childNodes    = theNode.getChildNodes;
+    numChildNodes = childNodes.getLength;
+    for c=1:numChildNodes
+        theChild  = childNodes.item(c-1);
+        childName = char(theChild.getNodeName);
+        if childName(1) == '#'
+            if ~isempty(char(theChild.getData))
+                switch childName(2:end)
+                    case 'text'
+                        node.get_data = char(theChild.getData);
+                    otherwise
+                        warning('Unknown type %s',childName)
+                end
+            end
+            continue
+        end
+        if ~isfield(node, childName)
+            idx = 1;
+        else
+            idx = numel(node.(childName)) + 1;
+        end
+        childStruct = parseNode(theChild);
+        fields = fieldnames(childStruct);
+        for f=1:numel(fields)
+            field = fields{f};
+            node.(childName)(idx).(field) = childStruct.(field);
+        end
+    end
+end
+node.get_attributes = parseAttributes(theNode);
+
+
+function attributes = parseAttributes(theNode)
+% Create attributes structure.
+
+attributes = containers.Map;
+if theNode.hasAttributes
+    attributeNodes = theNode.getAttributes;
+    numAttributes  = attributeNodes.getLength;
+    for c=1:numAttributes
+        theAttribute   = attributeNodes.item(c-1);
+        attributeName  = char(theAttribute.getName);
+        attributeValue = char(theAttribute.getValue);
+        attributes(attributeName) = attributeValue;
+    end
+end
