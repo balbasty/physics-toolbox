6d84bbe2bac486c9fc4b1a58fa0866943202ebba
Yaël Balbastre <yael.balbastre@gmail.com>
2018-11-09 19:18:59 +0000

    Feature: Possibility to encode sensitivities in DC space
    It may avoid overfitting truncation artefacts in low-dimensional images, where FMG's finite differences do not fare well.

--- multicoil_sensitivity.m
+++ multicoil_sensitivity.m
@@ -94,6 +94,9 @@ p.addParameter('VoxelSize',     [1 1 1],     @(X) isnumeric(X) && numel(X) <= 3)
 p.addParameter('LLPrior',       NaN,         @(X) isnumeric(X) && isscalar(X));
 p.addParameter('SensOptim',     [true true], @(X) (isnumeric(X) || islogical(X)) && numel(X) == 2);
 p.addParameter('Parallel',      0,           @(X) (isnumeric(X) || islogical(X)) && isscalar(X));
+p.addParameter('Encoding',      'frequency', @ischar);
+p.addParameter('NbBasis',       [10 10 10],  @(X) isnumeric(X) && numel(X) <= 3);
+p.addParameter('RegMatrix',     [],          @(X) isnumeric(X));
 p.parse(varargin{:});
 rho   = p.Results.MeanImage;
 x     = p.Results.CoilImages;
@@ -108,10 +111,14 @@ vs    = p.Results.VoxelSize;
 llp   = p.Results.LLPrior;
 optim = p.Results.SensOptim;
 Nw    = p.Results.Parallel;
+encoding  = p.Results.Encoding;
+nbasis    = p.Results.NbBasis;
+regmatrix = p.Results.RegMatrix;
 
 % -------------------------------------------------------------------------
 % Post-process input
-N = size(x,4);
+N   = size(x,4);
+lat = [size(x,1) size(x,2) size(x,3)];
 
 % Coils to process: default = all + ensure row-vector
 if isempty(all_n)
@@ -138,8 +145,16 @@ switch bnd
         warning('Unknown boundary condition %s. Using Neumann instead', num2str(bnd))
         bnd = 1;
 end
-% Voxel size: ensure row vector
-vs = double(vs(:)');
+% Nb basis: ensure row vector + complete
+nbasis = padarray(nbasis(:)', [0 max(0,3-numel(nbasis))], 'replicate', 'post');
+% Voxel size: ensure row vector + complete
+vs = padarray(vs(:)', [0 max(0,3-numel(vs))], 'replicate', 'post');
+% Create regularisation matrix
+if isempty(regmatrix)
+    regmatrix = spm_bias_lib('regulariser', prm, lat, nbasis, vs);
+end
+% Create basis functions (B)
+[B1,B2,B3] = spm_bias_lib('dcbasis', lat, nbasis);
 % Optimisation: if observed images are real, optim = [true false]
 if isrealarray(x)
     optim(2) = false;
@@ -166,8 +181,6 @@ spm_field('boundary', bnd);
 
 % -------------------------------------------------------------------------
 % Prepare stuff to save time in the loop
-% --- Dimensions
-lat = [size(x,1) size(x,2) size(x,3)];
 % --- GPU
 gpu_on = isa(A, 'gpuArray');
 if gpu_on, loadarray = @loadarray_gpu;
@@ -236,14 +249,20 @@ for n=all_n
     
     % ---------------------------------------------------------------------
     % Allocate conjugate gradient and Hessian
-    g   = zeros([lat sum(optim)], 'single');
-    H   = zeros([lat 1], 'single');
+    switch lower(encoding)
+        case 'image'
+            g   = zeros([lat sum(optim)], 'single');
+            H   = zeros(lat, 'single');
+        case 'frequency'
+            g   = zeros([nbasis sum(optim)], 'single');
+            H   = zeros([nbasis nbasis], 'single');
+    end
     llm = 0;
     
     % ---------------------------------------------------------------------
     % Compute gradient slice-wise to save memory
-    parfor(z=1:lat(3) , Nw) % < Uncomment for parallel processing
-    % for z=1:lat(3)          % < Uncomment for sequential processing
+    % parfor(z=1:lat(3) , Nw) % < Uncomment for parallel processing
+    for z=1:lat(3)          % < Uncomment for sequential processing
 
         % -----------------------------------------------------------------
         % Enforce boundary condition -> needed with parfor
@@ -251,17 +270,17 @@ for n=all_n
         
         % -----------------------------------------------------------------
         % Load one slice of the complete coil dataset
-        xz = loadarray(x(:,:,z,:), @single);
+        xz = loadarray(x(:,:,z,:), @double);
         xz = reshape(xz, [], N);
 
         % -----------------------------------------------------------------
         % Load one slice of the mean
-        rhoz = loadarray(rho(:,:,z,:), @single);
+        rhoz = loadarray(rho(:,:,z,:), @double);
         rhoz = reshape(rhoz, [], 1);
 
         % -----------------------------------------------------------------
         % Load one slice of the complete sensitivity dataset + correct
-        sz   = loadarray(s(:,:,z,:), @single);
+        sz   = loadarray(s(:,:,z,:), @double);
         sz   = reshape(sz, [], N);
         sz   = single(exp(double(sz)));
         rhoz = bsxfun(@times, rhoz, sz);
@@ -272,7 +291,7 @@ for n=all_n
         
         tmp = (rhoz - xz) * A;
         
-        llm = llm - 0.5 * sum(double(real(dot(tmp, rhoz - xz, 2))));
+        llm = llm - 0.5 * sum(real(dot(tmp, rhoz - xz, 2)));
         
         tmp = rhoz(:,n) .* conj(tmp(:,n));
         
@@ -288,56 +307,162 @@ for n=all_n
         
         Hz = A(n,n) * real(conj(rhoz(:,n)) .* rhoz(:,n));
         
-        g(:,:,z,:)  = reshape(gz, lat(1), lat(2), 1, []);
-        gz          = []; % clear
-        H(:,:,z)    = reshape(Hz, lat(1), lat(2));
-        Hz          = []; % clear
+        switch lower(encoding)
+            case 'image'
+                g(:,:,z,:)  = reshape(gz, lat(1), lat(2), 1, []);
+                gz          = []; % clear
+                H(:,:,z)    = reshape(Hz, lat(1), lat(2));
+                Hz          = []; % clear
+            case 'frequency'
+                b3z = B3(z,:);
+                gz  = reshape(gz, lat(1), lat(2), 1, []);
+                gz  = dct(gz, [], 1);
+                gz  = gz(1:nbasis(1),:,:,:);
+                gz  = dct(gz, [], 2);
+                gz  = gz(:,1:nbasis(2),:,:);
+                gz  = bsxfun(@times, gz, reshape(b3z, 1, 1, []));
+                g   = g + gz;
+                gz  = []; % clear
+                Hz  = reshape(Hz, lat(1), lat(2));
+                Hz  = kron(b3z'*b3z,spm_krutil(double(Hz),B1,B2,1));
+                H   = H + reshape(Hz, [nbasis nbasis]);
+                Hz  = []; % clear
+        end
         
         tmp  = []; % clear
         xz   = []; % clear
         rhoz = []; % clear
     end % < loop z
     
-    % ---------------------------------------------------------------------
-    % Gather gradient & Hessian (if on GPU)
-    g = gather(g);
-    H = gather(H);
     
-    % ---------------------------------------------------------------------
-    % Gradient: add prior term
-    if all(optim)
-        s0 = zeros([lat 2], 'single');
-        s1 = single(s(:,:,:,n));
-        s0(:,:,:,1) = real(s1);
-        s0(:,:,:,2) = imag(s1);
-        clear s1
-    elseif optim(1)
-        s0 = real(single(s(:,:,:,n)));
-    elseif optim(2)
-        s0 = imag(single(s(:,:,:,n)));
-    end
-    g  = g + spm_field('vel2mom', s0, [vs alpha(n)*prm], gamma(optim));
+    switch lower(encoding)
+        case 'image'
+            % -------------------------------------------------------------
+            % Gather gradient & Hessian (if on GPU)
+            g = gather(g);
+            H = gather(H);
+    
+            % -------------------------------------------------------------
+            % Gradient: add prior term
+            if all(optim)
+                s0 = zeros([lat 2], 'single');
+                s1 = single(s(:,:,:,n));
+                s0(:,:,:,1) = real(s1);
+                s0(:,:,:,2) = imag(s1);
+                clear s1
+            elseif optim(1)
+                s0 = real(single(s(:,:,:,n)));
+            elseif optim(2)
+                s0 = imag(single(s(:,:,:,n)));
+            end
+            g  = g + spm_field('vel2mom', s0, [vs alpha(n)*prm], gamma(optim));
 
-    % ---------------------------------------------------------------------
-    % Gauss-Newton
-    ds = zeros(size(s0), 'single');
-    i = 1;
-    if optim(1)
-        ds(:,:,:,i) = spm_field(H, g(:,:,:,i), [vs alpha(n)*prm 2 2], gamma(1));
-        i = i + 1;
-    end
-    if optim(2)
-        ds(:,:,:,i) = spm_field(H, g(:,:,:,i), [vs alpha(n)*prm 2 2], gamma(2));
-    end
-    clear g H
+            % -------------------------------------------------------------
+            % Gauss-Newton
+            ds = zeros(size(s0), 'single');
+            i = 1;
+            if optim(1)
+                ds(:,:,:,i) = spm_field(H, g(:,:,:,i), [vs alpha(n)*prm 2 2], gamma(1));
+                i = i + 1;
+            end
+            if optim(2)
+                ds(:,:,:,i) = spm_field(H, g(:,:,:,i), [vs alpha(n)*prm 2 2], gamma(2));
+            end
+            clear g H
+
+            % -------------------------------------------------------------
+            % Parts for log-likelihood (prior)
+            Lds = spm_field('vel2mom', ds, [vs alpha(n)*prm], gamma(optim));
+            llp_part1 = alpha(n) * double(reshape(s0, 1, [])) * double(reshape(Lds, [], 1));
+            llp_part2 = alpha(n) * double(reshape(ds, 1, [])) * double(reshape(Lds, [], 1));
+            clear s0 Lds
     
-    % ---------------------------------------------------------------------
-    % Parts for log-likelihood (prior)
-    Lds = spm_field('vel2mom', ds, [vs alpha(n)*prm], gamma(optim));
-    llp_part1 = alpha(n) * double(reshape(s0, 1, [])) * double(reshape(Lds, [], 1));
-    llp_part2 = alpha(n) * double(reshape(ds, 1, [])) * double(reshape(Lds, [], 1));
-    clear s0 Lds
-    
+        case 'frequency'
+            
+            % -------------------------------------------------------------
+            % Convert to vector/matrix
+            g = reshape(g, [], sum(optim));
+            g = double(g); 
+            
+            H = reshape(H, prod(nbasis), prod(nbasis));
+            H = double(H);
+            H = H + 1e-7 * max(diag(H)) * eye(size(H));
+            
+            % -------------------------------------------------------------
+            % Gradient: add prior term
+            if all(optim)
+                s0 = zeros([lat 2], 'single');
+                s1 = single(s(:,:,:,n));
+                s0(:,:,:,1) = real(s1);
+                s0(:,:,:,2) = imag(s1);
+                clear s1
+            elseif optim(1)
+                s0 = real(single(s(:,:,:,n)));
+            elseif optim(2)
+                s0 = imag(single(s(:,:,:,n)));
+            end
+            s0 = dct(s0,[],1);
+            s0 = s0(1:nbasis(1),:,:,:);
+            s0 = dct(s0,[],2);
+            s0 = s0(:,1:nbasis(2),:,:);
+            s0 = dct(s0,[],3);
+            s0 = s0(:,:,1:nbasis(3),:);
+            s0 = reshape(s0, [], sum(optim));
+            s0 = double(s0);
+            
+            i = 1;
+            if optim(1)
+                g(:,i) = g(:,i) + alpha(n) * gamma(1) * regmatrix * s0(:,i);
+                i = i + 1;
+            end
+            if optim(2)
+                g(:,i) = g(:,i) + alpha(n) * gamma(2) * regmatrix * s0(:,i);
+            end
+            
+            % -------------------------------------------------------------
+            % Gauss-Newton
+            ds = zeros(size(s0), 'double');
+            i  = 1;
+            if optim(1)
+                ds(:,i) = (H + alpha(n) * gamma(1) * regmatrix)\g(:,i);
+                i = i + 1;
+            end
+            if optim(2)
+                ds(:,i) = (H + alpha(n) * gamma(2) * regmatrix)\g(:,i);
+            end
+            clear g H
+            ds = reshape(ds, [nbasis sum(optim)]);
+            
+            % -------------------------------------------------------------
+            % Parts for log-likelihood (prior)
+            llp_part1 = 0;
+            llp_part2 = 0;
+            i = 1;
+            if optim(1)
+                Lds = alpha(n) * gamma(1) * regmatrix * reshape(ds(:,:,:,i), [], 1);
+                llp_part1 = llp_part1 + s0(:,i)' * Lds;
+                llp_part2 = llp_part2 + Lds' * Lds;
+                i = i + 1;
+            end
+            if optim(2)
+                Lds = alpha(n) * gamma(2) * regmatrix * reshape(ds(:,:,:,i), [], 1);
+                llp_part1 = llp_part1 + s0(:,i)' * Lds;
+                llp_part2 = llp_part2 + Lds' * Lds;
+            end
+            clear s0 Lds
+            
+            % -------------------------------------------------------------
+            % Convert to image representation
+            ds = single(ds);
+            % ds = idct(idct(idct(ds,lat(1),1),lat(2),2),lat(3),3);
+            ds = reshape(B1 * reshape(ds, nbasis(1), []), lat(1), nbasis(2), nbasis(3), []);
+            ds = permute(ds, [2 3 1 4]);
+            ds = reshape(B2 * reshape(ds, nbasis(2), []), lat(2), nbasis(3), lat(1), []);
+            ds = permute(ds, [2 3 1 4]);
+            ds = reshape(B2 * reshape(ds, nbasis(3), []), lat(3), lat(1), lat(2), []);
+            ds = permute(ds, [2 3 1 4]);
+    end
+            
     % ---------------------------------------------------------------------
     % Line-Search
     llm0   = llm;
