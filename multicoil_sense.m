aa8b64889c30c6023006f7c687b582051b1c7aa1
Yael Balbastre <y.balbastre@ucl.ac.uk>
2019-02-15 17:22:44 +0000

    Performance/Enhancement(sense): Remove unused param + Activate parfor

--- multicoil_sense.m
+++ multicoil_sense.m
@@ -12,8 +12,6 @@ end
 % -------------------------------------------------------------------------
 % Defaults
 sens        = [];
-sensmsk     = [];
-ssq         = [];
 A           = 1;
 coilorder   = {'ch' 'rd' 'k1' 'k2' 'av' 'sl' 'ct' 'st' 'sg'};
 sensorder   = {'k1' 'k2' 'rd' 'ch'};
@@ -26,6 +24,7 @@ fov_recon   = NaN;
 contrasts   = 1;
 senscontrast= 1;
 verbose     = 0;
+parallel    = 0;
 
 % -------------------------------------------------------------------------
 % Parse input
@@ -33,8 +32,6 @@ p = inputParser;
 p.FunctionName = 'multicoil_sense';
 p.addRequired('CoilKSpace',                       @(X) ischar(X) || isarray(X));
 p.addParameter('SensMaps',          sens,         @isarray);
-p.addParameter('SensMask',          sensmsk,      @isarray);
-p.addParameter('SumSquare',         ssq,          @isarray);
 p.addParameter('Precision',         A,            @isnumeric);
 p.addParameter('CoilOrder',         coilorder,    @iscell);
 p.addParameter('SensOrder',         sensorder,    @iscell);
@@ -46,26 +43,34 @@ p.addParameter('ReconMatrix',       lat_recon,    @isnumeric);
 p.addParameter('ReconFOV',          fov_recon,    @isnumeric);
 p.addParameter('Contrast',          contrasts,    @isnumeric);
 p.addParameter('SensContrast',      senscontrast, @isnumeric);
+p.addParameter('Parallel',          parallel,     @isboolean);
 p.addParameter('Verbose',           verbose,      @isboolean);
 p.parse(varargin{:});
-rdata       = p.Results.CoilKSpace;
-sens        = p.Results.SensMaps;
-sensmsk     = p.Results.SensMask;
-ssq         = p.Results.SumSquare;
-af          = p.Results.Acceleration;
-A           = p.Results.Precision;
-coilorder   = p.Results.CoilOrder;
-sensorder   = p.Results.SensOrder;
-coilcompact = p.Results.CoilCompact;
-lat_acq     = p.Results.AcquisitionMatrix;
-fov_acq     = p.Results.AcquisitionFOV;
-lat_recon   = p.Results.ReconMatrix;
-fov_recon   = p.Results.ReconFOV;
-contrasts   = p.Results.Contrast;
-senscontrast= p.Results.SensContrast(:)';
-verbose     = p.Results.Verbose;
+rdata        = p.Results.CoilKSpace;
+sens         = p.Results.SensMaps;
+af           = p.Results.Acceleration;
+A            = p.Results.Precision;
+coilorder    = p.Results.CoilOrder;
+sensorder    = p.Results.SensOrder;
+coilcompact  = p.Results.CoilCompact;
+lat_acq      = p.Results.AcquisitionMatrix;
+fov_acq      = p.Results.AcquisitionFOV;
+lat_recon    = p.Results.ReconMatrix;
+fov_recon    = p.Results.ReconFOV;
+contrasts    = p.Results.Contrast;
+senscontrast = p.Results.SensContrast(:)';
+parallel     = p.Results.Parallel;
+verbose      = p.Results.Verbose;
 
-filteredsens = ~isempty(ssq);
+
+% Parallel processing
+if isnan(parallel)
+    parallel = 0;
+end
+if ~isfinite(parallel)
+    parallel = parcluster('local');
+    parallel = parallel.NumWorkers;
+end
 
 % -------------------------------------------------------------------------
 % Estimate senstivity
@@ -96,14 +101,9 @@ end
 % -------------------------------------------------------------------------
 % Convert log-sensitivity to frequency (Neumann conditions -> dct)
 sens    = permute(sens, [4 1 2 3]); % < order: [ch k1 k2 rd]
-if filteredsens
-    sensmsk = permute(sensmsk, [4 1 2 3]); % < order: [ch k1 k2 rd]
-    ssq     = permute(ssq, [4 1 2 3]); % < order: [ch k1 k2 rd]
-else
-    sens    = dct(dct(dct(sens, [], 2), [], 3), [], 4);
-end
-dimsens  = size(sens);
-dimsens  = dimsens(2:4);
+sens    = dct(dct(dct(sens, [], 2), [], 3), [], 4);
+dimsens = size(sens);
+dimsens = dimsens(2:4);
 
 for contrast=contrasts
 
@@ -141,17 +141,8 @@ weights = double(msksp(abs(msksp) > eps('single')));
 rdata = fftshift(ifft(ifftshift(rdata,4), [], 4), 4);
 % > idct in readout direction (done once and for all)
 
-if filteredsens
-    sens = padarray(sens, [0 0 0 (recon_lat(3) - size(sens,4))/2]);
-    sens = fftshift(ifft(ifftshift(sens), [], 4));
-else
-    sens = idct(sens, recon_lat(3), 4);
-    sens = sens * sqrt(recon_lat(3)) / sqrt(dimsens(3));
-    
-    % This is just so that parfor does not complain
-    sensmsk = zeros([0 0 0 recon_lat(3)], 'single');
-    ssq     = zeros([0 0 0 recon_lat(3)], 'single');
-end
+sens = idct(sens, recon_lat(3), 4);
+sens = sens * sqrt(recon_lat(3)) / sqrt(dimsens(3));
 
 % % > debug: test on a few slices
 % rdata = rdata(:,:,:,76:95);
@@ -164,8 +155,7 @@ end
 % Reconstruction
 recon = zeros(recon_lat(1)*recon_lat(2),recon_lat(3), 'like', single(1i));
 ncoil = size(rdata,1);
-% for z=1:recon_lat(3)
-parfor(z=1:recon_lat(3), 15)
+parfor(z=1:recon_lat(3), parallel)
     % Acquired: read one slice (and expand to full matrix)
     xz = zeros(ncoil,recon_lat(1),recon_lat(2),'like', double(1i));
     xz(:,1:af(1):end,1:af(2):end) = rdata(:,:,:,z);
@@ -174,18 +164,11 @@ parfor(z=1:recon_lat(3), 15)
     % Sensitivity: read one slice 
     sz = double(sens(:,:,:,z));
     % Frequency -> Image
-    if ~filteredsens
-        sz = idct(idct(sz,recon_lat(1),2),recon_lat(2),3);
-        sz = sz * sqrt(recon_lat(1)) / sqrt(dimsens(1)) ...
-                * sqrt(recon_lat(2)) / sqrt(dimsens(2));
-        % Exponentiate
-        sz = exp(sz);
-    else
-        sz = padarray(sz, [0 (recon_lat(1:2) - dimsens(1:2))/2]);
-        sz = fftshift(ifft(ifft(ifftshift(sz), [], 2), [], 3));
-        sz = bsxfun(@rdivide, sz, ssq(:,:,:,z));
-        sz = bsxfun(@times,   sz, sensmsk(:,:,:,z));
-    end
+    sz = idct(idct(sz,recon_lat(1),2),recon_lat(2),3);
+    sz = sz * sqrt(recon_lat(1)) / sqrt(dimsens(1)) ...
+            * sqrt(recon_lat(2)) / sqrt(dimsens(2));
+    % Exponentiate
+    sz = exp(sz);
     % Whiten
     xz = reshape(xz, ncoil, []);
     sz = reshape(sz, ncoil, []);
